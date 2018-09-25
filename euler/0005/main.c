d5e19c51a743a59862adfcb48ea284ea3777a3f3
Bartłomiej Kurek <dev@metaceptron.com>
2018-09-25 21:50:03 +0200

    problem 5

---
+++ euler/0005/main.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+
+int main(void)
+{
+	unsigned long found = 0;
+	unsigned long i = 2520;
+
+	while(!found && i < ULONG_MAX) {
+		if(i % 20 == 0 && /* 10, 5, 4, 2 */
+		   i % 19 == 0 &&
+		   i % 18 == 0 && /* 9, 6, 3 */
+		   i % 17 == 0 &&
+		   i % 16 == 0 && /* 8, 4 */
+		   i % 15 == 0 && /* 5, 3 */
+		   i % 14 == 0 && /* 7, 2 */
+		   i % 13 == 0 &&
+		   i % 12 == 0 && /* 6, 4, 3, 2 */
+		   i % 11 == 0)
+		{
+			found = i;
+		}
+
+		i++;
+	}
+
+	printf("Found: %ld\n", found);
+
+	// check it:
+	int divisor;
+	for(divisor = 2; divisor <= 20; divisor++) {
+		if(found % divisor) {
+			printf("ERROR: Not divisible by: %d\n", divisor);
+			return 1;
+		}
+		else {
+			printf("OK: %ld %% %d = %d\n", found, divisor, (int)(found % divisor));
+		}
+	}
+
+	return 0;
+}

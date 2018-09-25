750c2fdd87914acbf399d1dcc8692a93d2c1cfa6
Bartłomiej Kurek <dev@metaceptron.com>
2018-09-25 16:08:02 +0200

    euler - prime factorization (naive algos)

--- euler/0003/main.c
+++ euler/0003/main.c
@@ -1,10 +1,40 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 
-long next_prime(long bottom)
+int is_prime(unsigned long n)
+/* naive */
+{
+	if(n < 2) {
+		return 0;
+	}
+	unsigned long max = n / 2;
+	unsigned long i;
+	for(i = 2; i <= max; i++) {
+		if(n % i == 0)
+			return 0;
+	}
+
+	return 1;
+}
+
+
+long next_prime(unsigned long bottom)
 /* naive prime gen. */
 {
-	while(
+	long i = 1;
+	long n = bottom;
+
+	while(i < 70000000) { // proven to be bounded...
+		n = bottom + i;
+		if(is_prime(n)) {
+			return n;
+		}
+
+		i++;
+	}
+
+	return -1;
 }
 
 
@@ -12,6 +42,22 @@ int main(int argc, char *argv[])
 {
 	char *ptr;
 	unsigned long number = (argc > 1 ? strtoul(argv[1], &ptr, 10) :  600851475143);
+
+	long prime = 2;
+	int pos = 0;
+
+	while(number > 1) {
+		if(number % prime == 0) {
+			printf("%ld, ", prime);
+			pos++;
+			number /= prime;
+		}
+		else {
+			prime = next_prime(prime);
+		}
+	}
+
+	printf("\n");
 
 	return 0;
 }

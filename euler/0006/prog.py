c12e7966f5a153a348cccce683fe14ea4b6a7379
Bartłomiej Kurek <dev@metaceptron.com>
2018-09-27 00:17:27 +0200

    py one liner

--- euler/0006/prog.py
+++ euler/0006/prog.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 
+# Regular version
+
 sq = 0
 s = 0
 for i in range(1, 100 + 1):
@@ -8,3 +10,10 @@ for i in range(1, 100 + 1):
     sq += i * i
 
 print(s * s - sq)
+
+# LisPy version
+(s, sq) = map(sum, zip(*[(_, _ * _) for _ in range(1, 100 + 1)] ))
+print(s * s - sq)
+
+# Perl Golf version.
+print([(k * k - v) for k,v in [list(map(sum, zip(*[(_, _ * _) for _ in range(1, 100 + 1)] )))]][0])
